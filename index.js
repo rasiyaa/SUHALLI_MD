dd2b4ecd136491444253c3e2c5c7f13b446d8477
efeurhobopatricia <169153471+efeurhobopatricia@users.noreply.github.com>
2024-09-25 11:45:48 +0100

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bot = require(__dirname + '/lib/smd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Suhail ${VERSION}`)
+    Debug.info(`SUHALLI_MD ${VERSION}`)
   try {
     await bot.init()
     bot.logger.info('⏳ Database syncing!')
